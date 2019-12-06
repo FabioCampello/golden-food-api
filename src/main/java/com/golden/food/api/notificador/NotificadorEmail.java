84433df75612589f5787f9f5de6f0b8da58c9953
fabiocampello@outlook.com.br <fabiocampello@outlook.com.br>
2019-12-05 23:32:05 -0300

    Módulo 02 - 2.14. Conhecendo os pontos de injeção e a anotação @Autowired

--- src/main/java/com/golden/food/api/notificador/NotificadorEmail.java
+++ src/main/java/com/golden/food/api/notificador/NotificadorEmail.java
@@ -1,31 +1,15 @@
 package com.golden.food.api.notificador;
 
+import org.springframework.stereotype.Component;
+
 import com.golden.food.api.modelo.Cliente;
 
+@Component
 public class NotificadorEmail implements Notificador {
-
-	private boolean caixaAlta;
-	private String hostServidorSmtp;
-
-	public NotificadorEmail() {
-		System.out.println("Construtor NotificadorEmail chamado.");
-	}
-
-	public NotificadorEmail(String hostServidorSmtp) {
-		this.hostServidorSmtp = hostServidorSmtp;
-		System.out.println("NotificadorEmail");
-	}
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		if (Boolean.TRUE.equals(this.caixaAlta)) {
-			mensagem = mensagem.toUpperCase();
-		}
-		System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n", cliente.getNome(), cliente.getEmail(), hostServidorSmtp, mensagem);
-	}
-
-	public void setCaixaAlta(boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
+		System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
 }
--- src/main/java/com/golden/food/api/service/AtivacaoClienteService.java
+++ src/main/java/com/golden/food/api/service/AtivacaoClienteService.java
@@ -1,16 +1,20 @@
 package com.golden.food.api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.golden.food.api.modelo.Cliente;
 import com.golden.food.api.notificador.Notificador;
 
+@Component
 public class AtivacaoClienteService {
-		
+	
+	@Autowired
 	private Notificador notificadorEmail;
 
-	public AtivacaoClienteService(Notificador notificadorEmail) {
-		this.notificadorEmail = notificadorEmail;
-		System.out.println("AtivacaoClienteService: " + notificadorEmail);
-	}
+//	public AtivacaoClienteService(Notificador notificadorEmail) {
+//		this.notificadorEmail = notificadorEmail;
+//	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
